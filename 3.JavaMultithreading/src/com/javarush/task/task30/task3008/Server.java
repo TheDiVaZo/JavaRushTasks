019867ea44d86eeae3c13aebee4a5762937ed292
Dima Klovnykh <70421596+TheDiVaZo@users.noreply.github.com>
2022-08-02 19:41:28 +0600

    Task: Create Chat (5)
    Writing class "Server" and listening connections
    Work with sockets

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int port = ConsoleHelper.readInt();
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("сервер запущен.");
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            } catch (Exception e) {
+                serverSocket.close();
+                e.printStackTrace();
+                break;
+            }
+        }
     }
 }
