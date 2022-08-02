0df9e87feceb7c08d0f41bfdb9b372949defb4b7
Dima Klovnykh <70421596+TheDiVaZo@users.noreply.github.com>
2022-08-02 17:27:52 +0600

    Task: Create Chat (2)
    Writing ConsoleHelper class

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,36 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String readedLine;
+        while (true) {
+            try {
+                readedLine = bufferedReader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return readedLine;
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.valueOf(readString());
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
 }
