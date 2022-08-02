f36b7f33d1bf19f85a28a54610af274a1fb57111
Dima Klovnykh <70421596+TheDiVaZo@users.noreply.github.com>
2022-08-02 19:49:18 +0600

    Task: Create Chat (7)
    write method "sendBroadcastMessage(Message)" in Server's Class and create connection's Map list

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,12 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
     private static class Handler extends Thread {
         private Socket socket;
@@ -12,6 +16,16 @@ public class Server {
         public Handler(Socket socket) {
             this.socket = socket;
         }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        connectionMap.values().forEach((connection -> {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                System.out.println("Сообщение не отправелось.");
+            }
+        }));
     }
 
     public static void main(String[] args) throws IOException {
