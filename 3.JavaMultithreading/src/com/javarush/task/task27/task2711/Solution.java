206f36174864f91731f470eeee457085d6c6c9c8
Dima Klovnykh <70421596+TheDiVaZo@users.noreply.github.com>
2022-08-05 21:13:23 +0600

    Task: CountDownLatch
    CountDownLatch

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task27.task2711;
+
+import java.util.concurrent.CountDownLatch;
+
+/* 
+CountDownLatch
+*/
+
+public class Solution {
+    private volatile boolean isWaitingForValue = true;
+
+    CountDownLatch latch = new CountDownLatch(1);
+
+    public void someMethod() throws InterruptedException {
+            while (isWaitingForValue) {
+                latch.await();
+
+            retrieveValue();
+
+            isWaitingForValue = false;
+            latch.countDown();
+        }
+    }
+
+    void retrieveValue() {
+        System.out.println("Value retrieved.");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
