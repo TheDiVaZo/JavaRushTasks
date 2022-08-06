783514080b7caafd39a39eac5765cf7d0f20a766
Dima Klovnykh <70421596+TheDiVaZo@users.noreply.github.com>
2022-08-06 12:52:40 +0600

    Task: Create Chat (21)
    Write ClientGuiController

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiController.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task30.task3008.client;
+
+public class ClientGuiController extends Client{
+    private ClientGuiModel model = new ClientGuiModel();
+    private ClientGuiView view = new ClientGuiView(this);
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    @Override
+    public void run() throws InterruptedException {
+        getSocketThread().run();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+    public ClientGuiModel getModel() {
+        return model;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new ClientGuiController().run();
+    }
+
+    public class GuiSocketThread extends SocketThread {
+        @Override
+        public void processIncomingMessage(String message) {
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        public void informAboutAddingNewUser(String userName) {
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        public void informAboutDeletingNewUser(String userName) {
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        public void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+}
